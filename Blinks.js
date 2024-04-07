85ea041565c7b5fc1a919962c49a17c4567d9622
Derryblinks <db723252@gmail.com>
2024-04-07 01:43:08 +0000

    startup websites

---
+++ Blinks.js
@@ -0,0 +1,10 @@
+//This is your first code in java script and i assure you that you are gonna make it to the end.
+let Derrick= 50;
+let Michael= Derrick + 20;
+console.log(Michael);
+console.log("i like pizza");
+windows.alert("i like pizza");
+
+//this is a single line comment and it takes the same form as that of the c++
+/* This is also a multiple line comment in javascript andit acts the sameway as that of the c++*/
+return 0;
